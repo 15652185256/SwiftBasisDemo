9da80ad9b64a9b913ce0585507db0a19d327e88e
15652185256 <15652185256@163.com>
2016-05-12 15:51:12 +0800

    函数修改

--- SwiftBasisDemo/AppDelegate.swift
+++ SwiftBasisDemo/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         
-        let navigationVC = UINavigationController(rootViewController: L05ArrayViewController());
+        let navigationVC = UINavigationController(rootViewController: L09FuncViewController());
         self.window?.rootViewController = navigationVC;
         self.window?.makeKeyAndVisible();
         
--- SwiftBasisDemo/L09FuncViewController.swift
+++ SwiftBasisDemo/L09FuncViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Foundation
 
 class L09FuncViewController: UIViewController {
 
@@ -14,16 +15,16 @@ class L09FuncViewController: UIViewController {
         super.viewDidLoad()
 
         //函数
-        sayHello("zhaoxd")
+        sayHello(姓名: "zhaoxd")
         
         let (a,b) = getNums()
         print(a,b)
         
         
         let fun = sayHello
-        fun("zhaoxd")
+        fun(姓名: "zhaoxd")
         
-        //函数带返回值
+        //函数带返回值 函数作为常量
         let num = ls([40,12,-5,78,98])
         print("Largest number is: \(num.large) and smallest number is: \(num.small)")
         
@@ -36,21 +37,68 @@ class L09FuncViewController: UIViewController {
         }
         
         
-        //参数可变型参数
+        
+        
+        
+        //可变参数
         vari("Swift", "Enumerations", "Closures")
+        
+        print(求合(12,114,121,121,11))
+        print(求合(1,24,3,12))
+        
+        
+        
+        
+        
+        
+        
         
         //常量，变量和 I/O 参数
         var no = 2
         var co = 10
-        temp(&no, b1: &co)
+        交换值(&no, b1: &co)
         print("Swapped values are \(no), \(co)")
+
+        //变量参数
+        居右对齐("00", 长度: 4, 填充字符: "123")
+        
+        
+        
+        
+        
+        //函数类型作为变量
+        var 临时 = 两个数相加
+        
+        print("2加5是\(临时(1,b: 5))")
+        
+        临时 = 两个数相乘
+        
+        print("2乘5是\(临时(1,b: 5))")
+        
+        
+        //函数类型作为参数
+        显示相乘结果(2, b: 5, 相乘函数: 两个数相乘)
+        
+        //函数类型作为返回值
+        var a1 = 10
+        let b1 = 判断加减(a1 > 0)
+        while a1 != 0 {
+            print(a1)
+            a1 = b1(a1)
+        }
+        
+        
+        
+        
         
         //嵌套函数
         let decrem = calcDecrement(forDecrement: -30)
         print(decrem())
+        
+        
     }
     
-    func sayHello(name:String){
+    func sayHello(姓名 name:String){
         print("Hello \(name)")
     }
     
@@ -98,20 +146,93 @@ class L09FuncViewController: UIViewController {
     }
     
     
-    //参数可变型参数
+    
+    
+    
+    //可变参数
     func vari<N>(members: N...){
         for i in members {
             print(i)
         }
     }
     
+    func 求合(数字集:Int...) -> Int {
+        var 和 = 0
+        for 数字 in 数字集 {
+            和 += 数字
+        }
+        return 和
+    }
+    
+    
+    
+    
+    
+    
     
     //常量，变量和 I/O 参数
-    func temp(inout a1: Int, inout b1: Int) {
+    func 交换值(inout a1: Int, inout b1: Int) {
         let t = a1
         a1 = b1
         b1 = t
     }
+    
+    //变量参数
+    func 居右对齐(var 原文本:String, 长度:Int, 填充字符:String) {
+        
+        let 要填充的长度 =  长度 - 原文本.characters.count
+        
+        for _ in 1...要填充的长度 {
+            原文本 = 原文本 + 填充字符
+        }
+        
+        print(原文本)
+    }
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //函数类型作为变量
+    func 两个数相加 (a:Int, b:Int) -> Int {
+        return a + b
+    }
+
+    func 两个数相乘 (a:Int, b:Int) -> Int {
+        return a * b
+    }
+    
+    //函数类型作为参数
+    func 显示相乘结果 (a:Int, b:Int, 相乘函数: (Int,Int) -> Int) {
+        let 结果 = 相乘函数(a, b)
+        print("\(a),\(b),\(结果)")
+    }
+    
+    
+    //函数类型作为返回值
+    func 加一(数字:Int) -> Int {
+        return 数字 + 1
+    }
+    
+    func 减一(数字:Int) -> Int {
+        return 数字 - 1
+    }
+    
+    func 判断加减(正:Bool) -> ((Int) -> Int) {
+        return 正 ? 减一 : 加一
+    }
+    
+    
+    
     
     //嵌套函数
     func calcDecrement(forDecrement total: Int) -> () -> Int {
@@ -122,7 +243,10 @@ class L09FuncViewController: UIViewController {
         }
         return decrementer
     }
-
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
