8b3ac678d334ad9ddead91209587dcd10332b9ec
15652185256 <15652185256@163.com>
2016-05-13 10:21:15 +0800

    闭包

--- SwiftBasisDemo/ClosuresViewController.swift
+++ SwiftBasisDemo/ClosuresViewController.swift
@@ -7,14 +7,51 @@
 //
 
 import UIKit
+import Foundation
 
 class ClosuresViewController: UIViewController {
+    
+    let 城市列表 = ["1","22","333","4444","55555"];
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.view.backgroundColor = UIColor.whiteColor()
+        
+        
+        //Sort函数用法
+        var 新城市列表 = 城市列表.sort({ (字符串1: String, 字符串2: String) -> Bool in
+            return 字符串1 > 字符串2
+        })
+        
+        print(新城市列表)
+        
+        
+        新城市列表 = 城市列表.sort({ 字符串1, 字符串2 in return 字符串1 < 字符串2 })
+        
+        print(新城市列表)
+        
+        
+        新城市列表 = 城市列表.sort({ 字符串1, 字符串2 in 字符串1 > 字符串2 })
+        
+        print(新城市列表)
+        
+        
+        新城市列表 = 城市列表.sort({ $0 < $1 })
+        
+        print(新城市列表)
+        
+        
+        新城市列表 = 城市列表.sort(>)
+        
+        print(新城市列表)
     }
+    
+    
+    //sorted函数
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
